b73b41afbdeffa90b8b032ac49e15244ad7eec85
Narender Surabhi <iamnarenderrao@gmail.com>
2024-10-28 02:51:12 -0400

    Initial commit

---
+++ kafka-consumer-opensearch/src/main/java/org/surabhin/kafka/opersearch/OpenSearchConsumer.java
@@ -0,0 +1,157 @@
+package org.surabhin.kafka.opersearch;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.bulk.BulkResponse;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.CreateIndexResponse;
+import org.opensearch.cluster.metadata.Metadata;
+import org.opensearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class OpenSearchConsumer {
+
+    public static RestHighLevelClient createOpenSearchClient() {
+        String connString = "http://localhost:9200";
+//        String connString = "https://c9p5mwld41:45zeygn9hy@kafka-course-2322630105.eu-west-1.bonsaisearch.net:443";
+
+        // we build a URI from the connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connString);
+        // extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // REST client without security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+
+        }
+
+        return restHighLevelClient;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        //Create an opensearch client
+        Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getName());
+
+        KafkaConsumer<String, String> kafkaConsumer = createKafkaConsumer("wikimedia.recentchange");
+
+        Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            public void run(){
+                log.info("Detected a shutdown, let's exit by calling consumer.wakeup()...");
+                kafkaConsumer.wakeup();
+
+                try{
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    log.error("Unexpected error: ", e);
+                }
+            }
+        });
+        //Kafka Client
+        try (RestHighLevelClient opensearchClient = createOpenSearchClient(); kafkaConsumer) {
+            //create an index if it does not exist already
+//            CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
+//            CreateIndexResponse createIndexResponse = opensearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+
+            while(true) {
+
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(10000 * 60));
+                BulkRequest bulkRequest = new BulkRequest();
+                for(ConsumerRecord<String, String> consumerRecord: consumerRecords) {
+
+                    try {
+                        String id = extractId(consumerRecord.value());
+                        IndexRequest indexRequest = new IndexRequest("wikimedia")
+                                .source(consumerRecord.value(), XContentType.JSON)
+                                .id(id);
+//                        IndexResponse indexResponse = opensearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                        bulkRequest.add(indexRequest);
+//                        log.info(indexResponse.getId());
+                    } catch (Exception e) {
+                        log.error("Error creating index on opensearch", e);
+                    }
+                }
+                if(bulkRequest.numberOfActions()>0) {
+                    BulkResponse bulkResponse = opensearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+                    log.info("Updated " + bulkResponse.getItems().length + " record(s).");
+                }
+            }
+
+
+        } catch (WakeupException wakeupException) {
+            log.info("Shutdown detected. Trying to shutdown.");
+        } catch (Exception e) {
+            log.error("Error: ", e);
+        }
+    }
+
+    private static String extractId(String value) {
+        return JsonParser.parseString(value)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
+    }
+
+    private static KafkaConsumer<String, String> createKafkaConsumer(String topic) {
+        String groupId = "my-application";
+        //Create Properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
+
+        // Create Consumer
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(Collections.singletonList(topic));
+        return  kafkaConsumer;
+    }
+}
